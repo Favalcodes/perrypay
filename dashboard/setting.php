e89c58a07d7f793acf2747ec9b6d701924cdb115
Favour Godwin Arua <favourvalg@gmail.com>
2021-03-04 21:29:22 +0100

    dashboard done

---
+++ dashboard/setting.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Dashboard | Classbeam</title>
+    <script src="assets/js/jquery-1.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/theme.css">
+    <style type="text/css">
+
+    </style>
+    <script>
+        function ajx(v, b) {
+            var ee;
+            if (window.XMLHttpRequest) {
+                ee = new XMLHttpRequest();
+            } else {
+                ee = new ActiveXObject("Microsoft.XMLHTTP");
+            };
+            ee.open("POST", 'dash.php', true);
+            ee.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+            ee.onreadystatechange = function() {
+                if (ee.status == 200 && ee.readyState == 4) {
+                    if (typeof b == "function") {
+                        if (ee.responseText == "unauth") {
+                            //do something here
+                        } else {
+                            b(ee.responseText);
+                        }
+                    };
+                }
+            }
+            ee.send(v);
+            return true;
+        };
+    </script>
+</head>
+
+<body>
+    <div class="overlay">
+        <form class="save_x">
+
+        </form>
+    </div>
+    <div class='sidebar'>
+        <div href='' onclick='window.location = this.getAttribute("href")' class='logo'><img src="../images/perry.png" alt="" class="perry-logo" width="100" height="100"></div>
+        <ul class="maxsid">
+            <li><i class="fa fa-home"></i><a href="index.php" class="nav-link"> Home</a></li>
+            <li><i class="fa fa-user"></i><a href="profile.php" class="nav-link"> Profile</a></li>
+            <li><i class="fa fa-bar-chart-o"></i><a href="sale.php" class="nav-link"> Sales</a></li>
+            <li class="chosen"><i class="fa fa-gear"></i><a href="setting.php" class="nav-link"> Settings</a></li>
+            <li><i class="fa fa-sign-out"></i><a href="logout.php" class="nav-link"> Logout</a></li>
+        </ul>
+    </div>
+    <div class='content'>
+        <div class="setting-content">
+            <div class="row">
+                <div class="col-md-6">
+                    <h6 class="mb-3">Change Password</h6>
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">New Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Confirm Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword">
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Sign me out</label>
+                        </div>
+                        <button type="submit" class="coin-btn">Submit</button>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <h6 class="mb-3">Change Email</h6>
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">New Email</label>
+                            <input type="email" class="form-control" id="exampleInputPassword">
+                        </div>
+                        <button type="submit" class="coin-btn">Submit</button>
+                    </form>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h6 class="mb-3 mt-2">Change Bank Details</h6>
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">New Bank</label>
+                            <input type="text" class="form-control" id="exampleInputPassword">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">New Account Number</label>
+                            <input type="text" class="form-control" id="exampleInputPassword">
+                        </div>
+                        <button type="submit" class="coin-btn">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<!-- Modals -->
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Understood</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+</html>
---
+++ head.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Stock Broker - provide the easy responsive free website templates. You can easily customize and make your own website for your startup business.">
+    <meta name="keywords" content="bootstrap template, Responsive Template, Website Template, free website templates, free website template download ">
+    <title>PerryPay</title>
+    <!-- Bootstrap -->
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- animsition css -->
+    <link rel="stylesheet" type="text/css" href="css/animsition.min.css">
+    <!-- Font Awesome CSS -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- font css -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <!-- owl Carousel Css -->
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+    <!-- Theme Css -->
+    <link rel="stylesheet" href="css/theme.css">
+</head>
